3d1acada29da62623d1c5a0c3c31e5f0ac653a4b
tkdgns8234 <tkdgns82345@naver.com>
2026-01-16 18:51:10 +0900

    init: spring project

---
+++ src/test/java/com/findy/FindyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
