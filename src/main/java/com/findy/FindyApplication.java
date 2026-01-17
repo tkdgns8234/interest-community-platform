3d002deb80c89835db95082ae422dadf730c78c0
tkdgns8234 <tkdgns82345@naver.com>
2026-01-17 18:05:36 +0900

    config: application config

--- src/main/java/com/findy/FindyApplication.java
+++ src/main/java/com/findy/FindyApplication.java
@@ -8,7 +8,6 @@ public class FindyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FindyApplication.class, args);
-        System.out.println("Hello World!");
     }
 
 }
